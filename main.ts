d97369018de33cadf651d4ef9de899e2903732c8
Kyle Lyver <144264052+kyle-lyver@users.noreply.github.com>
2023-11-22 12:03:30 -0500

    lis moi

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Kyle Lyver
+ * Created on: Nov 2023
+ * This program turns a 2 - 28BYJ-48 DC 5V until a sonar sensor senses an object 10cm away
 */
 
 basic.showString('Hello, World!')
