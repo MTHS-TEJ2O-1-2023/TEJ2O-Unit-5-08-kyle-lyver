d94614a2a6ad4a8981f0ac3ae68d4ae94dcb0aa0
Kyle Lyver <144264052+kyle-lyver@users.noreply.github.com>
2023-11-22 12:28:30 -0500

    twypeswiptt

--- main.ts
+++ main.ts
@@ -5,4 +5,29 @@
  * This program turns a 2 - 28BYJ-48 DC 5V until a sonar sensor senses an object 10cm away
 */
 
-basic.showString('Hello, World!')
+// variable
+let distanceToObject: number = 0
+
+// loop forever
+while (true) {
+  if (input.buttonIsPressed(Button.A) == true) {
+    // get sonar distance 
+    distanceToObject = sonar.ping(
+        DigitalPin.P1,
+        DigitalPin.P2,
+        PingUnit.Centimeters
+    )
+    basic.showIcon(IconNames.Yes)
+    basic.pause(1000)
+    basic.clearScreen()
+    // if distance > 10cm move wheels
+    if ((distanceToObject) > 10) {
+      robotbit.StpCarMove(10, 48)
+      basic.showIcon(IconNames.Heart)
+    }
+    if ((distanceToObject) <= 10) {
+      
+      basic.showIcon(IconNames.Duck)
+    }
+  }
+}
