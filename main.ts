4fd499b312a8766659852b32500774886c4f4351
Kyle Lyver <144264052+kyle-lyver@users.noreply.github.com>
2023-11-24 12:31:58 -0500

    erugbeas

--- main.ts
+++ main.ts
@@ -5,29 +5,31 @@
  * This program turns a 2 - 28BYJ-48 DC 5V until a sonar sensor senses an object 10cm away
 */
 
-// variable
 let distanceToObject: number = 0
 
-// loop forever
+// setup
+basic.showIcon(IconNames.Happy)
+
 while (true) {
-  if (input.buttonIsPressed(Button.A) == true) {
-    // get sonar distance 
-    distanceToObject = sonar.ping(
-        DigitalPin.P1,
-        DigitalPin.P2,
-        PingUnit.Centimeters
-    )
-    basic.showIcon(IconNames.Yes)
-    basic.pause(1000)
-    basic.clearScreen()
-    // if distance > 10cm move wheels
-    if ((distanceToObject) > 10) {
-      robotbit.StpCarMove(10, 48)
-      basic.showIcon(IconNames.Heart)
+    if (input.buttonIsPressed(Button.A) === true) {
+        // find distance from sonar
+        basic.clearScreen()
+        distanceToObject = sonar.ping(
+            DigitalPin.P1,
+            DigitalPin.P2,
+            PingUnit.Centimeters
+        )
+        basic.showNumber(distanceToObject)
+        basic.showIcon(IconNames.Happy)
+
+        if (distanceToObject < 10) {
+            robotbit.StpCarMove(-10, 48)
+            basic.pause(500)
+            robotbit.StepperTurn(robotbit.Steppers.M1, robotbit.Turns.T1B4)
+            basic.pause(500)
+            robotbit.StepperTurn(robotbit.Steppers.M2, robotbit.Turns.T1B4)
+            basic.pause(500)
+            robotbit.StpCarMove(10, 48)
+        }
     }
-    if ((distanceToObject) <= 10) {
-      
-      basic.showIcon(IconNames.Duck)
-    }
-  }
 }
