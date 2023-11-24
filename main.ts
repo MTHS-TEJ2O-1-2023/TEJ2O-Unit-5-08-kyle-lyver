1aee19908ac920b7a3be00d8265beb9afc7efc84
Kyle Lyver <144264052+kyle-lyver@users.noreply.github.com>
2023-11-24 12:40:11 -0500

    b;ju

--- main.ts
+++ main.ts
@@ -11,25 +11,25 @@ let distanceToObject: number = 0
 basic.showIcon(IconNames.Happy)
 
 while (true) {
-    if (input.buttonIsPressed(Button.A) === true) {
-        // find distance from sonar
-        basic.clearScreen()
-        distanceToObject = sonar.ping(
-            DigitalPin.P1,
-            DigitalPin.P2,
-            PingUnit.Centimeters
-        )
-        basic.showNumber(distanceToObject)
-        basic.showIcon(IconNames.Happy)
+  if (input.buttonIsPressed(Button.A) === true) {
+    // find distance from sonar
+    basic.clearScreen()
+    distanceToObject = sonar.ping(
+        DigitalPin.P1,
+        DigitalPin.P2,
+        PingUnit.Centimeters
+    )
+    basic.showNumber(distanceToObject)
+    basic.showIcon(IconNames.Happy)
 
-        if (distanceToObject < 10) {
-            robotbit.StpCarMove(-10, 48)
-            basic.pause(500)
-            robotbit.StepperTurn(robotbit.Steppers.M1, robotbit.Turns.T1B4)
-            basic.pause(500)
-            robotbit.StepperTurn(robotbit.Steppers.M2, robotbit.Turns.T1B4)
-            basic.pause(500)
-            robotbit.StpCarMove(10, 48)
-        }
+    if (distanceToObject < 10) {
+      robotbit.StpCarMove(-10, 48)
+      basic.pause(500)
+      robotbit.StepperTurn(robotbit.Steppers.M1, robotbit.Turns.T1B4)
+      basic.pause(500)
+      robotbit.StepperTurn(robotbit.Steppers.M2, robotbit.Turns.T1B4)
+      basic.pause(500)
+      robotbit.StpCarMove(10, 48)
     }
+  }
 }
