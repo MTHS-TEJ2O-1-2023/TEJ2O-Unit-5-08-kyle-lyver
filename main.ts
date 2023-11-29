6275f7ced97013badb24b21bd2da6e5c064452a9
Kyle Lyver <144264052+kyle-lyver@users.noreply.github.com>
2023-11-29 13:26:26 -0500

    bgrse

--- main.ts
+++ main.ts
@@ -15,25 +15,24 @@ while (true) {
     basic.clearScreen()
     while (true) {
     // find distance from sonar
-    distanceToObject = sonar.ping(
+      distanceToObject = sonar.ping(
         DigitalPin.P1,
         DigitalPin.P2,
         PingUnit.Centimeters
-    )
-    // basic.showNumber(distanceToObject)
-    // basic.pause(500)
-    // basic.showIcon(IconNames.Happy)
-    if (distanceToObject < 10) {
-      robotbit.StpCarMove(-10, 48)
-      basic.pause(500)
-      robotbit.StepperTurn(robotbit.Steppers.M1, robotbit.Turns.T1B4)
-      basic.pause(500)
-      robotbit.StepperTurn(robotbit.Steppers.M2, robotbit.Turns.T1B4)
-      basic.pause(500)
-      robotbit.StpCarMove(10, 48)
-    } else {
-      // move forward
-      robotbit.StpCarMove(10, 48)
+      )
+      // basic.showNumber(distanceToObject)
+      // basic.pause(500)
+      // basic.showIcon(IconNames.Happy)
+      if (distanceToObject < 10) {
+        robotbit.StpCarMove(-10, 48)
+        basic.pause(500)
+        robotbit.StepperTurn(robotbit.Steppers.M1, robotbit.Turns.T1B4)
+        basic.pause(500)
+        robotbit.StepperTurn(robotbit.Steppers.M2, robotbit.Turns.T1B4)
+        basic.pause(500)
+        robotbit.StpCarMove(10, 48)
+      } else {
+        robotbit.StpCarMove(10, 48)
       }
     }
   }
