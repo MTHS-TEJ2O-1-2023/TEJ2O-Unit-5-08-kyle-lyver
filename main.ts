e6422205840b74593abaa777e16a67703543249b
Kyle Lyver <144264052+kyle-lyver@users.noreply.github.com>
2023-11-29 12:32:06 -0500

    gdetg

--- main.ts
+++ main.ts
@@ -12,27 +12,27 @@ basic.showIcon(IconNames.Happy)
 
 while (true) {
   if (input.buttonIsPressed(Button.A) == true) {
-      // find distance from sonar
-      basic.clearScreen()
-      basic.clearScreen()
-      distanceToObject = sonar.ping(
-          DigitalPin.P1,
-          DigitalPin.P2,
-          PingUnit.Centimeters
-      )
-      basic.showNumber(distanceToObject)
-      basic.pause(500)
-      basic.showIcon(IconNames.Happy)
-      // if more than 10cm then move forwards
-      if (distanceToObject >= 10) {
-          robotbit.StpCarMove(10, 48)
-      }
-      // if less than 10cm turn and continue
-      if (distanceToObject < 10) {
-          robotbit.StepperTurn(robotbit.Steppers.M1, robotbit.Turns.T1B4)
-          robotbit.StepperTurn(robotbit.Steppers.M2, robotbit.Turns.T1B4)
-          basic.pause(500)
-          robotbit.StpCarMove(10, 48)
-      }
+    // find distance from sonar
+    basic.clearScreen()
+    basic.clearScreen()
+    distanceToObject = sonar.ping(
+      DigitalPin.P1,
+      DigitalPin.P2,
+      PingUnit.Centimeters
+    )
+    basic.showNumber(distanceToObject)
+    basic.pause(500)
+    basic.showIcon(IconNames.Happy)
+    // if more than 10cm then move forwards
+    while (distanceToObject >= 10) {
+      robotbit.StpCarMove(10, 48)
     }
+    // if less than 10cm turn and continue
+    while (distanceToObject < 10) {
+      robotbit.StepperTurn(robotbit.Steppers.M1, robotbit.Turns.T1B4)
+      robotbit.StepperTurn(robotbit.Steppers.M2, robotbit.Turns.T1B4)
+      basic.pause(500)
+      robotbit.StpCarMove(10, 48)
+    }
+  }
 }
